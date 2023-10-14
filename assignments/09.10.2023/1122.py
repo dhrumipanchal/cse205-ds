ab684850db6935b686b3c2b099c0f168200c775e
dhrumipanchal <dhrumi.p3@ahduni.edu.in>
2023-10-14 21:59:21 +0530

    LAB-5

---
+++ assignments/09.10.2023/1122.py
@@ -0,0 +1,9 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        result=[]
+        for i in arr2:
+            while i in arr1:
+                result.append(i)
+                arr1.remove(i)
+        
+        return result+sorted(arr1)
