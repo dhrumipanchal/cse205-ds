ecbcbdcaf4f8d041fc3c3939db5e8e92dfbff014
dhrumipanchal <dhrumi.p3@ahduni.edu.in>
2023-10-16 14:31:18 +0530

    LAB-6

---
+++ assignments/16.10.2023/290.py
@@ -0,0 +1,14 @@
+dic = {}
+        s = s.split()
+        if len(set(pattern)) != len(set(s)):
+            return False
+        if len(pattern) != len(s):
+            return False
+        for i in range(len(pattern)):
+            if pattern[i] not in dic:
+                dic[pattern[i]] = s[i]
+            elif dic[pattern[i]] == s[i]:
+                continue
+            else:
+                return False
+        return True
